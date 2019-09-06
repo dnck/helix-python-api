5a13e68ec90a82ba3f01ae050b3563958e25b903
Dan Cook <cook@Dans-MacBook-Pro.local>
2019-09-06 16:15:55 +0200

    Generalize get_node_info example

--- bin/examples/get_node_info.py
+++ bin/examples/get_node_info.py
@@ -1,13 +1,26 @@
+import argparse
+
 from context import api
 
-HOST = 'coo.hlxtest.net'
-HTTP_PORT = 8085
 
-NODE_HTTP_ENDPOINT = "http://{}:{}".format(HOST, HTTP_PORT)
-API_CLIENT = api.BaseHelixAPI()
-
-def get_node_info():
-    response = API_CLIENT.get_node_info(NODE_HTTP_ENDPOINT)
+def get_node_info(node_http_endpoint):
+    response = API_CLIENT.get_node_info(node_http_endpoint)
     print(response)
 
-get_node_info()
+if __name__ == '__main__':
+    API_CLIENT = api.BaseHelixAPI()
+
+    PARSER = argparse.ArgumentParser(description='Get info from a node.')
+    PARSER.add_argument('-host',
+        metavar='host', type=str, default='coo.hlxtest.net',
+        help='Public IP of the host'
+    )
+    PARSER.add_argument('-port',
+        metavar='port', type=str, default='8085',
+        help='HTTP port of the host public IP'
+    )
+    ARGS = PARSER.parse_args()
+
+    NODE_HTTP_ENDPOINT = "http://{}:{}".format(ARGS.host, ARGS.port)
+
+    get_node_info(NODE_HTTP_ENDPOINT)

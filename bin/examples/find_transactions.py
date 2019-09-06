7b2c97c51bb967f40b3eb0f0eeb17e7452fde0f8
Dan Cook <cook@Dans-MacBook-Pro.local>
2019-09-06 17:09:10 +0200

    Generalize find_transactions in examples

--- bin/examples/find_transactions.py
+++ bin/examples/find_transactions.py
@@ -1,14 +1,29 @@
+import argparse
+
 from context import api
 
-HOST = 'coo.hlxtest.net'
-HTTP_PORT = 8085
 
-NODE_HTTP_ENDPOINT = "http://{}:{}".format(HOST, HTTP_PORT)
-API_CLIENT = api.BaseHelixAPI()
-
-def test_find_transactions():
-    addresses = ['9474289ae28f0ea6e3b8bedf8fc52f14d2fa9528a4eb29d7879d8709fd2f6d37']
-    response = API_CLIENT.find_transaction(NODE_HTTP_ENDPOINT, addresses)
+def _test_find_transactions(node_http_endpoint,addresses=['0'*64]):
+    response = API_CLIENT.find_transaction(node_http_endpoint, addresses)
     print(len(response['hashes']))
 
-test_find_transactions()
+
+if __name__ == '__main__':
+    API_CLIENT = api.BaseHelixAPI()
+
+    PARSER = argparse.ArgumentParser(description='Get info from a node.')
+    PARSER.add_argument('-host',
+        metavar='host', type=str, default='coo.hlxtest.net',
+        help='Public IP of the host'
+    )
+    PARSER.add_argument('-port',
+        metavar='port', type=str, default='8085',
+        help='HTTP port of the host public IP'
+    )
+    ARGS = PARSER.parse_args()
+
+    NODE_HTTP_ENDPOINT = "http://{}:{}".format(ARGS.host, ARGS.port)
+
+    address = ['9474289ae28f0ea6e3b8bedf8fc52f14d2fa9528a4eb29d7879d8709fd2f6d37']
+
+    _test_find_transactions(NODE_HTTP_ENDPOINT,addresses=address)

2efe76e9e99d7c52569b0aa974f754cf55df6386
Dan Cook <cook@Dans-MacBook-Pro.local>
2019-09-06 16:22:10 +0200

    Generalize get_neighbors example

--- bin/examples/get_neighbors.py
+++ bin/examples/get_neighbors.py
@@ -1,16 +1,28 @@
+import argparse
+
 from context import api
 
-HOST = 'coo.hlxtest.net'
-HTTP_PORT = 8085
-
-NODE_HTTP_ENDPOINT = "http://{}:{}".format(HOST, HTTP_PORT)
-API_CLIENT = api.BaseHelixAPI()
-
-def get_node_info():
-    response = API_CLIENT.get_neighbors(NODE_HTTP_ENDPOINT)
+def _get_neighbors(node_http_endpoint):
+    response = API_CLIENT.get_neighbors(node_http_endpoint)
     neighbors = response['neighbors']
     for node in neighbors:
         print(node)
         print('')
 
-get_node_info()
+if __name__ == '__main__':
+    API_CLIENT = api.BaseHelixAPI()
+
+    PARSER = argparse.ArgumentParser(description='Get info from a node.')
+    PARSER.add_argument('-host',
+        metavar='host', type=str, default='coo.hlxtest.net',
+        help='Public IP of the host'
+    )
+    PARSER.add_argument('-port',
+        metavar='port', type=str, default='8085',
+        help='HTTP port of the host public IP'
+    )
+    ARGS = PARSER.parse_args()
+
+    NODE_HTTP_ENDPOINT = "http://{}:{}".format(ARGS.host, ARGS.port)
+
+    _get_neighbors(NODE_HTTP_ENDPOINT)

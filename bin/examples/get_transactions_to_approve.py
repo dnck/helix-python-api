7b91c67e783f6b92e5e9633c2dfac2807a292327
Dan Cook <cook@Dans-MacBook-Pro.local>
2019-09-06 16:41:16 +0200

    Generalize get_transactions_to_approve.py

--- bin/examples/get_transactions_to_approve.py
+++ bin/examples/get_transactions_to_approve.py
@@ -1,14 +1,28 @@
+import argparse
+
 from context import api
 
-HOST = 'coo.hlxtest.net'
-HTTP_PORT = 8085
 
-NODE_HTTP_ENDPOINT = "http://{}:{}".format(HOST, HTTP_PORT)
-API_CLIENT = api.BaseHelixAPI()
-
-def get_transactions_to_approve():
-    depth = 3
-    response = API_CLIENT.get_transactions_to_approve(NODE_HTTP_ENDPOINT, depth)
+def _get_transactions_to_approve(node_http_endpoint):
+    response = API_CLIENT.get_transactions_to_approve(
+        node_http_endpoint, 3 # depth = 3
+    )
     print(response)
 
-get_transactions_to_approve()
+if __name__ == '__main__':
+    API_CLIENT = api.BaseHelixAPI()
+
+    PARSER = argparse.ArgumentParser(description='Get info from a node.')
+    PARSER.add_argument('-host',
+        metavar='host', type=str, default='coo.hlxtest.net',
+        help='Public IP of the host'
+    )
+    PARSER.add_argument('-port',
+        metavar='port', type=str, default='8085',
+        help='HTTP port of the host public IP'
+    )
+    ARGS = PARSER.parse_args()
+
+    NODE_HTTP_ENDPOINT = "http://{}:{}".format(ARGS.host, ARGS.port)
+
+    _get_transactions_to_approve(NODE_HTTP_ENDPOINT)
